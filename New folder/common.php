5533a07ecb6651527d1abc8559d375a909d7e492
Sarthak <77429824+sarthakdevil@users.noreply.github.com>
2023-12-04 21:59:16 +0530

    Add files via upload

--- New folder/common.php
+++ New folder/common.php
@@ -1,9 +1,9 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $database = "blog";
+    $servername = "sql12.freemysqlhosting.net";
+    $username = "sql12667523";
+    $password = "MmRBiNWmaX";
+    $database = "sql12667523";
     $conn = mysqli_connect($servername, $username, $password, $database);
 }
     ?>
